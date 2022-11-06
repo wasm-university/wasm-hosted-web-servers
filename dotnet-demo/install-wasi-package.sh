803daf53c0785e84bd62fe36c2703f104d675940
Philippe Charrière <ph.charriere@gmail.com>
2022-11-06 20:26:42 +0000

    📝 save

--- dotnet-demo/install-wasi-package.sh
+++ dotnet-demo/install-wasi-package.sh
@@ -2,6 +2,6 @@
 
 export DOTNET_ROOT=$HOME/dotnet
 export PATH=$PATH:$HOME/dotnet
-cd hello
+cd dotnet-demo/hello
 dotnet add package Wasi.Sdk --prerelease
 dotnet add package Wasi.AspNetCore.Server.Native --prerelease
